f731f9aabf96b9962d3d8b61c09b16e435373f34
ANUPA SAJI <36967384+ANUPASAJI@users.noreply.github.com>
2018-03-11 10:08:42 +0530

    Add files via upload
    Operations in java like addition substraction and multiplication

---
+++ operations.java
@@ -0,0 +1,15 @@
+/**A Sample program*/
+public class sample
+{
+public static void main(string args[])
+{
+int a,b,c=0,p=0;
+a=45;b=25;
+c=a+b;
+d=a-b;
+p=a*b;
+System.out.print("The sum of two numbers="+c);
+System.out.print("The difference of two numbers="+d);
+System.out.print("The product of two numbers="+p);
+}
+}
